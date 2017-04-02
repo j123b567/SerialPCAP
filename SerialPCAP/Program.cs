a1ddb6caccd81642b881046092176f83fe895e66
Jan Breuer <jan.breuer@jaybee.cz>
2017-04-02 13:05:54 +0200

    Refactor options parsing

--- SerialPCAP/Program.cs
+++ SerialPCAP/Program.cs
@@ -1,70 +1,107 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Ports;
+using Mono.Options;
 
 namespace SerialPCAP
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
+		static int baudRate = 9600;
+		static int frameGapMs = 10;
+		static uint dlt = 147;
+		static string portName;
+		static string outputFile;
+
+		static void ShowHelp(OptionSet options)
 		{
-			if (args.Length == 0)
+			Console.WriteLine("usage: serialpcap [options] <portName>");
+			options.WriteOptionDescriptions(Console.Out);
+			Console.WriteLine();
+			Console.WriteLine("Available portName:");
+			// Get a list of serial port names.
+			string[] ports = SerialPort.GetPortNames();
+			// Display each port name to the console.
+			foreach (string port in ports)
 			{
-				Console.WriteLine("usage: serialpcap <portName> [<baudRate> [<frameGapMs> [<pcapDLT>]]]");
-				Console.WriteLine();
-				Console.WriteLine("Available portName:");
-				// Get a list of serial port names.
-				string[] ports = SerialPort.GetPortNames();
-				// Display each port name to the console.
-				foreach (string port in ports)
-				{
-					Console.WriteLine("    " + port);
-				}
-				Console.WriteLine();
-				Console.WriteLine("baudRate: serial port speed (default 9600)");
-				Console.WriteLine();
-				Console.WriteLine("frameGapMs: inter frame gap in miliseconds (defailt 10)");
-				Console.WriteLine();
-				Console.WriteLine("pcapDLT: data link type in pcap format (defailt 147)");
+				Console.WriteLine("    " + port);
 			}
-			else {
-				string portName = args[0];
-				int baudRate = 9600;
-				if (args.Length >= 2) baudRate = Int32.Parse(args[1]);
-				int frameGapMs = 10;
-				if (args.Length >= 3) frameGapMs = Int32.Parse(args[2]);
-				uint dlt = 147;
-				if (args.Length >= 4) dlt = UInt32.Parse(args[3]);
+		}
 
-				string outputFile = "serial-" + DateTime.UtcNow.ToString("yyyyMMdd-HHmmss") + ".pcap";
+		static void RunCapture()
+		{
+			Console.WriteLine("Serial port: " + portName);
+			Console.WriteLine("Baud rate: " + baudRate + " Bd");
+			Console.WriteLine("Frame gap: " + frameGapMs + " ms");
+			Console.WriteLine("DLT: " + dlt);
+			Console.WriteLine("Output file: " + outputFile);
+			Console.WriteLine();
+			Console.WriteLine("Starting capture (press Ctrl+c to stop)");
 
-				Console.WriteLine("Serial port: " + portName);
-				Console.WriteLine("Baud rate: " + baudRate + " Bd");
-				Console.WriteLine("Frame gap: " + frameGapMs + " ms");
-				Console.WriteLine("DLT: " + dlt);
-				Console.WriteLine("Output file: " + outputFile);
-				Console.WriteLine();
-				Console.WriteLine("Starting capture (press Ctrl+c to stop)");
-
-				using (BinaryWriter writer = new BinaryWriter(File.Open(outputFile, FileMode.Create)))
+			using (BinaryWriter writer = new BinaryWriter(File.Open(outputFile, FileMode.Create)))
+			{
+				Pcap.Header.Write(writer, dlt);
+				using (var capture = new CaptureSerial(portName, baudRate, frameGapMs))
 				{
-					Pcap.Header.Write(writer, dlt);
-
-					using (var capture = new CaptureSerial(portName, baudRate, frameGapMs))
+					while (true)
 					{
-						while (true)
+						var packet = capture.CapturePacket();
+						if (packet != null)
 						{
-							var packet = capture.CapturePacket();
-							if (packet != null)
-							{
-								packet.Write(writer);
-								writer.Flush();
-							}
+							packet.Write(writer);
+							writer.Flush();
 						}
 					}
 				}
-
 			}
+		}
+
+		public static void Main(string[] args)
+		{
+			string outputFilePrefix = "";
+			bool shouldShowHelp = false;
+
+			var options = new OptionSet {
+				{"b|baud=", "Serial port speed (default 9600)", (int b) => baudRate = b},
+				{"g|gap=", "Inter frame gap in miliseconds (default 10)", (int g) => frameGapMs = g},
+				{"d|dlt=", "Data link type in pcap format (default 147)", (uint d) => dlt = d},
+				{"o|output=", "Output file prefix", o => outputFilePrefix = o},
+				{"h|help", "Show this message and exit", h => shouldShowHelp = h != null},
+			};
+
+			List<string> extra;
+			try
+			{
+				// parse the command line
+				extra = options.Parse(args);
+			}
+			catch (OptionException e)
+			{
+				// output some error message
+				Console.Write("serialpcap: ");
+				Console.WriteLine(e.Message);
+				Console.WriteLine("Try `serialpcap --help' for more information.");
+				return;
+			}
+
+			if (shouldShowHelp || extra.Count != 1)
+			{
+				ShowHelp(options);
+				return;
+			}
+
+			portName = extra[0];
+
+			if (outputFilePrefix == "")
+			{
+				var s = portName.Split(new char[] { '/', '\\' });
+				outputFilePrefix = s[s.Length - 1];
+			}
+
+			outputFile = outputFilePrefix + "-" + DateTime.UtcNow.ToString("yyyyMMdd-HHmmss") + ".pcap";
+
+			RunCapture();
 		}
 	}
 }
